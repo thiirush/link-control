d62ceedfe3fdaf53a8aeab39d0d0b40eb91452c6
Thiago Pereira <estudospereira@gmail.com>
2020-08-01 12:55:21 -0300

    Sign up implemented

--- frontend/src/screens/Signup/index.js
+++ frontend/src/screens/Signup/index.js
@@ -1,39 +1,61 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-const SignUp = () =>{
-    return (
-        <div className="container h-100 pt-5">
-            <h1>Cadastro</h1>
-            <div className="d-flex flex-column h-100">
-                <form>
-                    <div className="form-group">
-                        <label>Email</label>    
-                        <input type="text" className="form-control" />
-                    </div>
+import { signUp } from "./SignupActions";
 
-                    <div className="form-group">
-                        <label>Senha</label>    
-                        <input className="form-control" type="password" />
-                    </div>
+const SignUp = (props) => {
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-                    <div className="form-group">
-                        <label>Confirmação de senha</label>    
-                        <input className="form-control" type="password" />
-                    </div>
-                    <div>
-                    <button className="btn btn-primary btn-round">Enviar</button>
-                    </div>                        
-                </form>
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
 
-                <div className="container text-center fixed-bottom pb-5">
-                    <div className="text-muted">Já possui cadastro?</div>
-                    <Link to="/sign-in">Entrar</Link>
-                </div>
-                
-            </div>
+    console.log(data);
+
+    signUp(data);
+  };
+  return (
+    <div className="container h-100 pt-5">
+      <h1>Cadastro</h1>
+      <div className="d-flex flex-column h-100">
+        <form onSubmit={submitHandler}>
+          <div className="form-group">
+            <label>Email</label>
+            <input type="text" className="form-control" name="email" />
+          </div>
+
+          <div className="form-group">
+            <label>Senha</label>
+            <input className="form-control" type="password" name="password" />
+          </div>
+
+          <div className="form-group">
+            <label>Confirmação de senha</label>
+            <input
+              className="form-control"
+              type="password"
+              name="password_confirmation"
+            />
+          </div>
+          <div>
+            <button className="btn btn-primary btn-round">Enviar</button>
+          </div>
+        </form>
+
+        <div className="container text-center fixed-bottom pb-5">
+          <div className="text-muted">Já possui cadastro?</div>
+          <Link to="/sign-in">Entrar</Link>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default SignUp;
+const mapStateToProps = (state) => {
+  return {
+    account: state.signUp.account,
+  };
+};
+
+export default connect(mapStateToProps, { signUp })(SignUp);
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import ReduxPromise from "redux-promise";
 import SigninReducer from "./screens/Signin/SigninReducer";
+import SignupReducer from "./screens/Signup/SignupReducer";
 
 const reducers = combineReducers({
   signIn: SigninReducer,
+  signUp: SignupReducer,
 });
 
 const store = createStore(reducers, applyMiddleware(ReduxPromise));
